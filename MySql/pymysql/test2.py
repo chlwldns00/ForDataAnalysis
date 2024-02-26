127efc3006f08ad927f4b2ce984f1d2b6107cf38
chlwldns00 <chlwldns00@naver.com>
2024-02-26 22:21:09 +0900

    0226A

--- MySql/pymysql/test2.py
+++
@@ -1,5 +0,0 @@
-SQL = """INSERT INTO product_info VALUES("""+3+""",
-        '""" + 'like' + """','""" + 'you' + """','""" + 'somuch' + """',
-        '""" + 'good' + """',"""+2+ """);
-        """
-print(SQL)
